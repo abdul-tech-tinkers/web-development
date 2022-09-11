4efd840f1a247f8ff218e04d22fc55057322479e
ZakriyaBhatti <88224341+ZakriyaBhatti@users.noreply.github.com>
2022-09-11 18:44:24 +0530

    included unique id for react

--- React/contact-app/src/components/App.js
+++ React/contact-app/src/components/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const CONTACT_STORAGE_KEY = "contacts";
+
   const [contacts, setContacts] = useState([]);
   const addContact = (contact) => {
-    setContacts([...contacts, contact]);
+    const uniqueId = uuidv4();
+    console.log(uniqueId);
+    const newContact = { id: uniqueId, ...contact };
+    console.log(newContact);
+    setContacts([...contacts, newContact]);
   };
+
+  useEffect(() => {
+    localStorage.setItem(CONTACT_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  useEffect(() => {
+    const retContacts = JSON.parse(localStorage.getItem(CONTACT_STORAGE_KEY));
+    if (retContacts) setContacts(retContacts);
+  }, []);
 
   return (
     <div className="flex flex-col ml-[10%] mr-[10%] w-[80%]">
